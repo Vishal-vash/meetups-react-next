d1ec396ccbb255c31b3f5039aa733f4df0d69b2d
30117229 <vishal.vasishat@conduent.com>
2021-06-18 19:23:44 +0530

    Updated DB  Configuration

--- pages/api/db-utils.js
+++ pages/api/db-utils.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export const connectToMeetupCollections = async () => {
   const client = await MongoClient.connect(
-    "mongodb://vishal:nokian72@cluster0-shard-00-00.tywjg.mongodb.net:27017/meetups-db?ssl=true&replicaSet=atlas-or7lh3-shard-0&authSource=admin&retryWrites=true&w=majority"
+    `mongodb://${process.env.username}:${process.env.password}@cluster0-shard-00-00.tywjg.mongodb.net:27017/meetups-db?ssl=true&replicaSet=atlas-or7lh3-shard-0&authSource=admin&retryWrites=true&w=majority`
   );
   console.log("Connected Successfully.");
   const db = client.db();
--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,11 +5,6 @@ import { connectToMeetupCollections } from "./db-utils";
 async function handler(req, res) {
   if (req.method == "POST") {
     const data = req.body;
-    // const client = await MongoClient.connect("mongodb://vishal:nokian72@cluster0-shard-00-00.tywjg.mongodb.net:27017/meetups-db?ssl=true&replicaSet=atlas-or7lh3-shard-0&authSource=admin&retryWrites=true&w=majority");
-    // console.log("Connected Successfully.")
-    // const db = client.db();
-
-    // const meetupsCollection = db.collection('meetups');
 
     const {meetupsCollection, client} = await connectToMeetupCollections();
 
